82a37d4d763b1e56631d7045182dc5e6722b1618
K-ai30 <artpirate11@aim.com>
2020-03-21 21:12:28 -0400

    added route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,10 +4,27 @@ const express = require("express");
 const router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-let burger = require("../models/burger.js");
+let Burger = require("../models/burger.js");
 
 router.get("/", (req, res) => {
-    res.render("index");
+    Burger.all(function(data) {
+        res.render("index", {burgers: data});
+    })
+})
+
+router.post("/addBurger", (req, res) => {
+    let burgerName = req.body.burgerName;
+    Burger.create("burger_name", [burgerName], function(data) {
+        res.redirect("/");
+    })
+})
+
+router.post("/devourBurger", (req, res) => {
+    let id = req.body.burgerID;
+    let update = {devoured:1};
+    Burger.update(update, `id = ${id}`, function(data) {
+        res.redirect("/");
+    })
 })
 
 module.exports = router;
